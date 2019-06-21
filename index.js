fb1a43fb34a67ed56bb456ae379b8d1991f2f08b
Patrice Alex <patricealex.21@gmail.com>
2019-06-21 11:14:25 -0400

    updated syntax error in index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,() => {
   console.log(`example app listening on ${PORT}!`);
 })
-app.use(express.static(_dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 app.get('/', (request, response) => {
   response.sendFile(__dirname + 'public/index.html');
 });
